2fb235c73f2ac4d625d1cdbebf063e67de92170a
Mohammad Murtada <118128637+MYMurtada@users.noreply.github.com>
2023-07-05 16:00:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world!")
+# First we need to check for which function user wants.
+
+logicFunction = input('Enter logic function name \'and,or,not\' :')
+
+# -------------------------------------------
+# ------------------[DONE]-------------------
+# -------------------------------------------
